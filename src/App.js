b0f3fa6342c7d9a4508966a79da8c955ab86c21e
khanarezamaulana <khanarezamaulana@gmail.com>
2018-12-11 14:24:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  state =  { textBoxInput: "", randomTeks: "" }
+
+  randomPict = () => {
+    this.setState ({
+      randomTeks: this.state.textBoxInput
+    })
+  }
+
+  display() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <img className="rounded-circle bg-white my-4 mx-auto" src={`https://robohash.org/${this.state.randomTeks}.png`}/>
+    )
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <div className="container text-center p-5">
+          <input type="text" className="form-control w-25 mx-auto my-3" placeholder="Type your name..." onChange={(e) => { this.setState({ textBoxInput: e.target.value}) } }></input>
+          <button type="button" className="btn btn-success" onClick={ this.randomPict }>Buat Profil Picture!</button>
+          <div className="row">
+            <div className="col-lg-12 text-center"></div>
+              {this.state.randomTeks && this.display()}
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
-
 export default App;
